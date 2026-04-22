7b6320a958f186f640d22658743ea099f6d59eae
saifeddine19 <saifeddinealjane@gmail.com>
2026-04-22 11:13:54 +0200

    rectification du fichier de test de la classe voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -14,4 +14,35 @@ public class VoitureTest {
         assertEquals(1,1);
     }
 
+    @Test
+    void testConstructeur(){
+        Voiture v = new Voiture("Audi", 45000);
+
+        assertEquals("Audi", v.getMarque(), "La marque doit être Audi");
+
+        assertEquals(45000, v.getPrix(), "Le prix doit être 45000");
+    }
+
+    @Test
+    void testSetters() {
+        Voiture v = new Voiture();
+        v.setMarque("Toyota");
+        v.setPrix(15000);
+        v.setId(5);
+
+        assertEquals("Toyota", v.getMarque());
+        assertEquals(15000, v.getPrix());
+        assertEquals(5, v.getId());
+    }
+
+
+    @Test
+    void testToString() {
+        Voiture v = new Voiture("Tesla", 60000);
+        v.setId(1);
+        String attendu = "Car{marque='Tesla', prix=60000, id=1}";
+        assertEquals(attendu, v.toString());
+    }
+
+
 }
